8abd85efce393e6c80a5e9dfea3cf0a46888bd48
rinaldomelo <rinaldomelo72@gmail.com>
2016-05-30 04:07:14 -0300

    Database conection Sucess

---
+++ php_includes/db_conx.php
@@ -0,0 +1,14 @@
+<?php
+
+$db_conx = mysqli_connect("phpsocialnetwork.rinaldomelo.com", "rinaldomelo72", "phpsocialnetwork", "phpsocialnetwork");
+
+if (mysqli_connect_errno()) {
+    
+    echo mysqli_connect_error();
+    exit();
+} else {
+    
+    echo "Success";
+}
+
+?>
